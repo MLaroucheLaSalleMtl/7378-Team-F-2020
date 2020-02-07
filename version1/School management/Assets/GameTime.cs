747da3fb87dc0e7cd9a8d495d8de90af9ae87133
VanillaSpace <aohashi@live.com>
2020-02-07 10:30:03 -0500

    V6_070220
    School now has a time cycle

---
+++ version1/School management/Assets/GameTime.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTime : MonoBehaviour
+{
+    private const int Timescale = 45;
+
+    private Text clockTxt, seasonTxt, dayTxt,yearTxt;
+
+    private double minute, hour, day, second, month, year;
+
+    void Start()
+    {
+        month = 1;
+        day = 1;
+        year = 2000;
+        clockTxt = GameObject.Find("Clock").GetComponent<Text>();
+        clockTxt = GameObject.Find("Day").GetComponent<Text>();
+        clockTxt = GameObject.Find("Season").GetComponent<Text>();
+        clockTxt = GameObject.Find("Year").GetComponent<Text>();
+        //calculate the season
+        
+
+    }
+
+    
+    void Update()
+    {
+        CalculateTime();    
+    }
+
+    private void CalculateTime()
+    {
+        second += Time.deltaTime * Timescale;
+
+        if (second >= 60)
+        {
+            minute++;
+            second = 0;
+            UpdateText();
+        }else
+        if (minute >= 60)
+        {
+            hour++;
+            minute = 0;
+            UpdateText();
+        }
+        else if(hour >= 60)
+        {
+            day++;
+            hour = 0;
+            UpdateText();
+        }else if (day >= 28)
+        {
+            CalcMonth();
+        }else if(month >= 12)
+        {
+            month = 1;
+            year++;
+            UpdateText();
+            CalculateSeason();
+        }
+
+    }
+    private void CalculateSeason()
+    {
+        if (month == 1 || month == 3 || month == 5||month == 7 || month == 8 || month == 10 || month == 12 )
+
+        {
+            if (day >= 32)
+            {
+                month++;
+                day = 1;
+                UpdateText();
+                CalculateSeason();
+            }
+        }
+        if (month == 4 || month == 6 || month == 9 || month == 11)
+
+        {
+            if (day >= 31)
+            {
+                month++;
+                day = 1;
+                UpdateText();
+                CalculateSeason();
+            }
+        }
+        if (month == 2)
+
+        {
+            if (day >= 29)
+            {
+                month++;
+                day = 1;
+                UpdateText();
+                CalculateSeason();
+            }
+        }
+    }
+    private void CalcMonth()
+    {
+        if( month == 2 || month == 3|| month == 4)
+
+        {
+            seasonTxt.text = "Spring";
+        }
+        else if( month == 5 || month == 7|| month == 8|| month == 6)
+        {
+            seasonTxt.text = "Summer";
+            
+        }
+        else if ( month == 9||month == 10)
+        {
+            seasonTxt.text = "Autum";
+
+        }
+        else if (month == 1 || month == 11 || month == 12)
+        {
+            
+            seasonTxt.text = "Winter";
+        }
+    }
+
+    private void UpdateText()
+    {
+        dayTxt.text = "Day: " + day;
+        clockTxt.text = "Time: " + hour + ":" + minute;
+        yearTxt.text = "Year:" + year;
+    }
+}
