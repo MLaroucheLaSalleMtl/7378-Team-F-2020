ae924d6f42e78d4f28b50ab192229adfb1de6984
VanillaSpace <aohashi@live.com>
2020-02-23 21:02:40 -0500

    12.3_230220
    added some script to 3D teacher

---
+++ version1/School management/Assets/teacherAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class teacherAnim : MonoBehaviour
+{
+    private NavMeshAgent nav;
+    private Animator anim;
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedOfcharacter = nav.velocity.magnitude / nav.speed;
+        anim.SetFloat("Speed", speedOfcharacter, 1f, Time.deltaTime);
+    }
+}
