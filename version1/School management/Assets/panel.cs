5cb81d7fba13ff2f37eec1ab08326398ecda37dd
VanillaSpace <aohashi@live.com>
2020-02-24 23:03:03 -0500

    13.1_240220
    Added more GUI and fixed LOADING scene

---
+++ version1/School management/Assets/panel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class panel : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel !=null)
+        {
+            Panel.SetActive(false);
+        }
+
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
