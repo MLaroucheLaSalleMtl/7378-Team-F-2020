5cf6db7148e71d685473c563b0cf14a9abe7b108
VanillaSpace <aohashi@live.com>
2020-04-20 15:49:19 -0400

    backlog & music settings added

---
+++ version1/School management/Assets/musicBG.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class musicBG : MonoBehaviour
+{
+    [SerializeField] private AudioClip backgroundMusic; 
+
+    private AudioSource bgMusic; 
+
+    [SerializeField] private GameObject musicPanel;
+
+    bool panelActivation = false;
+
+
+    public void openPanel()
+    {
+        musicPanel.SetActive(true);
+        panelActivation = true;
+    }
+
+    public void closePanel()
+    {
+        musicPanel.SetActive(false);
+        panelActivation = false;
+    }
+
+    public void doPanel()
+    {
+        if (panelActivation)
+        {
+            closePanel();
+            Debug.Log("close ");
+        }
+        else
+        {
+            openPanel();
+            Debug.Log("open ");
+        }
+    }
+
+    public void PlayPauseMusic()
+    {
+        // Check if the music is currently playing.
+        if (bgMusic.isPlaying)
+            bgMusic.Pause(); // Pause if it is
+        else
+            bgMusic.Play(); // Play if it isn't
+    }
+
+    public void PlayStop()
+    {
+        if (bgMusic.isPlaying)
+            bgMusic.Stop();
+        else
+            bgMusic.Play();
+    }
+
+    public void PlayMusic()
+    {
+        bgMusic.Play();
+    }
+
+    public void StopMusic()
+    {
+        bgMusic.Stop();
+    }
+
+    public void PauseMusic()
+    {
+        bgMusic.Pause();
+    }
+
+    void Start()
+    {
+
+        bgMusic = GetComponent<AudioSource>();
+
+
+        bgMusic.clip = backgroundMusic;
+
+        bgMusic.volume = 0.010f;
+        PlayMusic();
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
